f6fea9d849d430eff3dccbebc21a06d40098ccfd
Shamsiddin <xamroqulovsh7@gmail.com>
2026-04-14 18:27:11 +0500

    Integrated Database and Prisma

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { InternalServerErrorException, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  try {
+    const app = await NestFactory.create(AppModule);
+    const PORT = process.env.PORT ?? 3000;
+    app.setGlobalPrefix('api');
+    app.useGlobalPipes(
+      new ValidationPipe({
+        transform: true,
+      }),
+    );
+    await app.listen(PORT, () =>
+      console.log(`Server is running on http://127.0.0.1:${PORT}`),
+    );
+  } catch (err) {
+    throw new InternalServerErrorException(err.message);
+  }
 }
 bootstrap();
