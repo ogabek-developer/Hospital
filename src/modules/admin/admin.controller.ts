b6a2a949759898259ec30cfcd49feb9a77cc3b49
Shamsiddin <xamroqulovsh7@gmail.com>
2026-04-15 16:01:47 +0500

    Added admin-crud

---
+++ src/modules/admin/admin.controller.ts
@@ -0,0 +1,39 @@
+import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post } from '@nestjs/common';
+import { AdminService } from './admin.service';
+import { CreateAdminDto } from './dto/create-admin.dto';
+import { UpdateAdminDto } from './dto/update-admin.dto';
+
+@Controller('admin')
+export class AdminController {
+  constructor(private readonly adminService: AdminService) {}
+
+  @Post()
+  @HttpCode(201)
+  create(dto: CreateAdminDto) {
+    return this.adminService.create(dto);
+  }
+
+  @Get()
+  @HttpCode(200)
+  findAll() {
+    return this.adminService.findAll();
+  }
+
+  @Get(':id')
+  @HttpCode(200)
+  findOne(@Param('id') id: number) {
+    return this.adminService.findOne(id);
+  }
+
+  @Patch(':id')
+  @HttpCode(200)
+  update(@Param('id') id: number, @Body() dto: UpdateAdminDto) {
+    return this.adminService.update(id, dto);
+  }
+
+  @Delete(':id')
+  @HttpCode(204)
+  remove(@Param('id') id: number) {
+    return this.adminService.remove(id);
+  }
+}
---
+++ src/modules/admin/admin.service.ts
@@ -0,0 +1,36 @@
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/providers/prisma/prisma.service';
+import { CreateAdminDto } from './dto/create-admin.dto';
+import { UpdateAdminDto } from './dto/update-admin.dto';
+
+@Injectable()
+export class AdminService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async create(dto: CreateAdminDto) {
+    const exist = await this.prismaService.admins.findUnique({where: {phone_number: dto.phone_number}});
+    if(exist) throw new ConflictException('Admin already exist');
+    const admin = await this.prismaService.admins.create({data: dto});
+    return admin;
+  }
+
+  async findAll() {
+    return this.prismaService.admins.findMany()
+  }
+
+  async findOne(id: number) {
+    const admin = await this.prismaService.admins.findUnique({where: {id}});
+    if(!admin) throw new NotFoundException('Admin not found');
+    return admin;
+  }
+
+  async update(id: number, dto: UpdateAdminDto) {
+    await this.findOne(id);
+    return this.prismaService.admins.update({where: {id}, data: dto});
+  }
+
+  async remove(id: number) {
+    await this.findOne(id);
+    return this.prismaService.admins.delete({where: {id}});
+  }
+}
